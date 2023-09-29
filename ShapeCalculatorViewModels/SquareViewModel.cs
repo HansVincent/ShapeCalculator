28e0251704160afbae7b3e03d642381c033aefef
HansVincent <vinvenjamin.gravinez.21@usjr.edu.ph>
2023-09-29 13:42:49 +0800

    Square Page and Circle left.

---
+++ ShapeCalculatorViewModels/SquareViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShapeCalculator.ShapeCalculatorViewModels
+{
+    internal class SquareViewModel : INotifyPropertyChanged
+    {
+        private int _side;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public int Side
+        {
+            get
+            {
+                return _side;
+            }
+            set
+            {
+                _side = value;
+                OnPropertyChanged(nameof(Side));
+            }
+        }
+        void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
